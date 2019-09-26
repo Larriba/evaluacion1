b2f626386b4ceaae237e0bd52b92d3a9117890ee
Larriba <55530876+Larriba@users.noreply.github.com>
2019-09-26 09:41:46 +0200

    Add files via upload

---
+++ saludador_2.py
@@ -0,0 +1,6 @@
+def saludador_2():
+    print"eyyy bro" #instruccion de salida
+    nombre=raw_input("como te llamas?")  #instruccion de entrada
+    print"bienvenido a Phyton" +" "+nombre
+    
+saludador_2()
