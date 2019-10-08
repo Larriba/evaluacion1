036bacf1f5254d9f930cfcb5ce14043fd616b8fb
Larriba <55530876+Larriba@users.noreply.github.com>
2019-10-08 11:48:52 +0200

    Add files via upload

---
+++ pare e impares.py
@@ -0,0 +1,14 @@
+def contador():
+    print "introduzca el numero"
+    a=input("ponga el numero aqui")
+    for i in range(1,a+1):
+        if (i%2==0):
+            print i," par"
+
+        else:
+            print i," impar"
+
+contador()
+    
+    
+    
